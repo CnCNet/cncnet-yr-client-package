b86c2e9e3e1ed842c0af9591965ecb02b4797ceb
Devin <devo1929@gmail.com>
2021-12-30 11:53:14 -0500

    YR Maps Updater .NET

---
+++ YRMapsUpdater/YRMapUpdater/Extensions/KeyDataCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using IniParser.Model;
+
+namespace YRMapUpdater.Extensions
+{
+    public static class KeyDataCollectionExtensions
+    {
+        public static void SetKeyData(this KeyDataCollection keys, string key, string value) =>
+            keys.SetKeyData(new KeyData(key)
+            {
+                Value = value
+            });
+
+        public static void SetKeyData(this KeyDataCollection keys, int key, string value) => keys.SetKeyData(key.ToString(), value);
+
+        public static void SetKeyData(this KeyDataCollection keys, int key, int value) => keys.SetKeyData(key.ToString(), value.ToString());
+
+        public static void SetKeyData(this KeyDataCollection keys, string key, int value) => keys.SetKeyData(key, value.ToString());
+
+        public static KeyData GetKeyData(this KeyDataCollection keys, string key) => keys.GetKeyData(key);
+
+        public static string GetKeyValue(this KeyDataCollection keys, string key) => keys.GetKeyData(key)?.Value;
+    }
+}
