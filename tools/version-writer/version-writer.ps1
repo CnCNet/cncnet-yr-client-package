7d422193fb5f24103651508b3e3df755d1e56a16
Devin <devo1929@gmail.com>
2023-02-15 11:16:57 -0500

    repo structure refactor

---
+++ tools/version-writer/version-writer.ps1
@@ -0,0 +1,25 @@
+﻿. $PSScriptRoot\..\base-config.ps1
+
+Write-Output "Running VersionWriter..."
+
+$VersionWriterExe = "VersionWriter.exe"
+$VersionWriterCopiedFiles = "$( $Config.PackagePath )\VersionWriter-CopiedFiles"
+$VersionFile = "$( $Config.PackagePath )\version"
+if (Test-Path $VersionFile)
+{
+    Remove-Item $VersionFile
+}
+$Process = Start-Process "$PSScriptRoot\$VersionWriterExe" "/S $( $Config.PackagePath )" -Wait -NoNewWindow
+if ($Process.ExitCode > 0)
+{
+    Throw "Failed to run VersionWriter.exe"
+}
+Remove-Item $VersionWriterCopiedFiles -Recurse -Force
+if ($Env:CI -eq "true")
+{
+    Write-Output "Running as CI/CD. Removing versionconfig.ini file."
+    $VersionConfigFile = "$( $Config.PackagePath )\versionconfig.ini"
+    Remove-Item $VersionConfigFile -Force
+}
+
+Write-Output "VersionWriter done."
