443ce023bec39415cab065c3ba0f4650cf9a38fc
Belonit <54427022+Belonit@users.noreply.github.com>
2025-10-31 20:57:26 +0300

    Reimplement GetFolderSize() in mix-packer
    Co-Authored-By: SadPencil <11227602+SadPencil@users.noreply.github.com>

--- tools/mix-packer/ExFS.ts
+++ tools/mix-packer/ExFS.ts
@@ -1,9 +1,7 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import * as glob from "glob";
-const fastFolderSizeSync = require("fast-folder-size/sync") as (
-    target: string
-) => number;
+const fastFolderSizeSync = require("fast-folder-size/sync") as (target: string) => number;
 
 function toPosixPath(pathStr: string) {
     return pathStr.replace(/\\/g, "/");
@@ -52,7 +50,34 @@ export default class ExFS {
     }
 
     static GetFolderSize(folderPath: string) {
-        return fastFolderSizeSync(folderPath) || 0;
+        if (!fs.existsSync(folderPath)) {
+            return 0;
+        }
+
+        try {
+            // Try the quick method
+            return fastFolderSizeSync(folderPath) || 0;
+        } catch {
+            try {
+                const globPattern = toPosixPath(path.join(folderPath, "**", "*"));
+                const filesList = glob.sync(globPattern, { nodir: true, absolute: true });
+
+                let totalSize = 0;
+                for (const currentFile of filesList) {
+                    try {
+                        const st = fs.statSync(currentFile);
+                        totalSize += st.size || 0;
+                    } catch {
+                        // ignore files that disappear / are inaccessible
+                        continue;
+                    }
+                }
+
+                return totalSize;
+            } catch {
+                return 0;
+            }
+        }
     }
 
     static GetFile(filePath: string) {
