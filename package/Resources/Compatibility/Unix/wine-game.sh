3a3d49bb1a5481449d143066c0e4c9b9bb697a76
Kerbiter <crabiter@vivaldi.net>
2026-05-31 17:12:16 +0300

    fix: wine game launch args

--- package/Resources/Compatibility/Unix/wine-game.sh
+++ package/Resources/Compatibility/Unix/wine-game.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-${WINE:=wine} Syringe.exe -i=Ares.dll -i=CnCNet-Spawner.dll -i=Phobos.dll \ "gamemd-spawn.exe" -SPAWN -LOG -CD -RA2ModeSaveID=0x8d113b94
+${WINE:=wine} Syringe.exe -i=Ares.dll -i=CnCNet-Spawner.dll -i=Phobos.dll \ "gamemd-spawn.exe" -SPAWN -LOG -CD -Include -Inheritance -RA2ModeSaveID=0x8d113b94
